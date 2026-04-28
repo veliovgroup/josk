9ec549dd3befba1aea86a4a1756363075a62ea3b
dr.dimitru <ceo@veliov.com>
2026-04-28 18:28:09 +0300

    📦 pre-v5

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'ostrio:cron-jobs',
-  version: '5.0.0',
+  version: '5.1.0',
   summary: 'Tasks/CRON scheduler and manager for horizontally scaled multi-server apps',
   git: 'https://github.com/veliovgroup/josk',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['1.6', '3.0-beta.0']);
-  api.use('ecmascript@0.16.8 || 0.16.8-beta300.0', 'server');
+  api.versionsFrom(['1.6', '3.0.0', '3.4']);
+  api.use('ecmascript', 'server');
   api.mainModule('index.js', 'server');
 });
 
@@ -17,8 +17,9 @@ Package.onTest((api) => {
     'cron-parser': '4.9.0',
     chai: '4.4.1',
     redis: '4.6.13',
+    pg: '8.13.1',
   });
 
-  api.use(['ecmascript@0.16.8 || 0.16.8-beta300.0', 'mongo@1.6.19 || 2.0.0-beta300.0', 'meteortesting:mocha@2.1.0 || 3.1.0-beta300.0'], 'server');
+  api.use(['ecmascript', 'mongo', 'meteortesting:mocha@3.3.0'], 'server');
   api.addFiles('test/meteor.js', 'server');
 });
